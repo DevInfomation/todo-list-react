5b46bfbc2f01a659707cf7c06c20d75cb4add27f
devinfo <devinformationproject@gmail.com>
2025-08-15 18:01:08 +0200

    style: creating the look of the todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -4,7 +4,10 @@ const Todo = () => {
   return (
     <div className='container mx-auto px-4 mt-10'>
         <h1 className='text-5xl'>Add Task</h1>
-        
+        <div>
+          <input className='border-1 rounded-md p-1 mt-5 w-96' type="text" placeholder='Enter a task'/>
+          <button className='bg-blue-400 p-1 ml-3 rounded-md text-white cursor-pointer hover:bg-blue-300'>Add Task</button>
+        </div>
     </div>
   )
 }
