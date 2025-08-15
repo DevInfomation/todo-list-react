9fe4911457ab24f611cd6c4795e9c4be2145c7d8
devinfo <devinformationproject@gmail.com>
2025-08-15 17:43:32 +0200

    first stage bby

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-blue-400 h-72 flex'>
+        <h1 className="text-black text-7xl font-sans self-center ml-10">To-Do List</h1>
+    </div>
+  )
+}
+
+export default Header
