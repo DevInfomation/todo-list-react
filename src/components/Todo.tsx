81780673176cc03bb6320c8acc0add0485993aaf
devinfo <devinformationproject@gmail.com>
2025-08-18 15:45:43 +0200

    style: adding the Task title

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const Todo = () => {
+  const [ task, setTask ] = useState('sup buddy');
+
   return (
     <div className='container mx-auto px-4 mt-10'>
         <h1 className='text-5xl'>Add Task</h1>
         <div>
-          <input className='border-1 rounded-md p-1 mt-5 w-96' type="text" placeholder='Enter a task'/>
+          <input value={task} onChange={e => setTask(e.target.value )} className='border-1 rounded-md p-1 mt-5 w-96' type="text" placeholder='Enter a task'/>
           <button className='bg-blue-400 p-1 ml-3 rounded-md text-white cursor-pointer hover:bg-blue-300'>Add Task</button>
+        </div>
+        <div>
+          <h1 className='text-5xl mt-5'>Tasks:</h1>
+          
         </div>
     </div>
   )
